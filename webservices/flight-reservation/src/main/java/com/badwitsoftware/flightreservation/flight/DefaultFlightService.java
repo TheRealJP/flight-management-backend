34e1469e4b349544fb2a65c9169e8b644bc750d4
Juan Ortiz <jportiz32@gmail.com>
2019-04-22 08:20:17 -0400

    Add application-local.properties

--- webservices/flight-reservation/src/main/java/com/badwitsoftware/flightreservation/flight/DefaultFlightService.java
+++ webservices/flight-reservation/src/main/java/com/badwitsoftware/flightreservation/flight/DefaultFlightService.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class FlightServiceImpl implements FlightService {
+public class DefaultFlightService implements FlightService {
 
 	@Autowired
 	private FlightRepository flightRepository;
