fd092dc4b0bf96bbea8b21384680610dc16292be
Juan Ortiz <jportiz32@gmail.com>
2019-10-08 09:33:59 -0400

    Closes #2
    Utilize JavaMailSenderImpl for email generation

--- flight-reservation/src/main/java/com/badwitsoftware/flightreservation/util/EmailGenerator.java
+++ flight-reservation/src/main/java/com/badwitsoftware/flightreservation/util/EmailGenerator.java
@@ -8,7 +8,9 @@ import javax.mail.internet.MimeMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
@@ -16,8 +18,14 @@ import org.springframework.stereotype.Component;
 public class EmailGenerator {
 
 	private static final Logger log = LoggerFactory.getLogger(EmailGenerator.class);
+
 	@Autowired
 	private JavaMailSender sender;
+
+	@Bean
+	private JavaMailSender sender() {
+		return new JavaMailSenderImpl();
+	}
 
 	public void sendItineraryToEmail(String email, String filePath) {
 		MimeMessage mimeMessage = sender.createMimeMessage();
