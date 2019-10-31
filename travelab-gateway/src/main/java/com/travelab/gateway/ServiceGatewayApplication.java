ed192f8ff6f379f755de07756b11360b6014b0a0
Juan Ortiz <jportiz32@gmail.com>
2019-10-31 16:51:06 -0400

    Add main function to gateway application

--- travelab-gateway/src/main/java/com/travelab/gateway/ServiceGatewayApplication.java
+++ travelab-gateway/src/main/java/com/travelab/gateway/ServiceGatewayApplication.java
@@ -3,14 +3,20 @@
  */
 package com.travelab.gateway;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
+
 @SpringBootApplication
 public class ServiceGatewayApplication {
 
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceGatewayApplication.class, args);
+	}
+	
 	@Bean
 	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
 		return builder
@@ -23,4 +29,5 @@ public class ServiceGatewayApplication {
 				.uri("http://localhost:8080"))
 			.build();
 	}
+	
 }
